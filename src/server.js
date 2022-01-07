c42613acde4ba01cc076cd170ffca1307184c681
Kangminhee <minheek99@gmail.com>
2022-01-08 03:00:45 +0900

    3.5

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
 
-const handleHome = (req,res) => {
-    return res.end();
+const gossipMiddleware = (req,res,next) => {
+    console.log(`Someone is going to : ${req.url}`);
+    next();
 }
 
+const handleHome = (req,res) => {
+    return res.send("I love middlewares!");
+};
+
 app.get("/",handleHome);
+app.use(gossipMiddleware);
 
 const handelListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT,handelListening);
-
-
