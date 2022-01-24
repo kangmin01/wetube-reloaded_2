b778ac8729e7ff310093758b698bb03f93666d65
Kangminhee <minheek99@gmail.com>
2022-01-25 02:51:33 +0900

    1.24

--- src/server.js
+++ src/server.js
@@ -4,13 +4,11 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
 
 app.use("/", globalRouter);
 app.use("/videos",videoRouter);
